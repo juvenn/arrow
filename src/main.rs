e5333ec2b9e6d00d108df6d5ebf4dcb789ca37c3
Juvenn Woo <machese@gmail.com>
2023-05-05 19:39:00 +0800

    Add sub command mode

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,38 @@ mod context;
 mod decode;
 mod pipeline;
 use anyhow::anyhow;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use context::Context;
 use pipeline::Pipelines;
+use std::env;
 use std::io;
 
-// Run pipelines defined in yml file, default to .arrow/*.yml
-#[derive(Parser, Debug)]
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
 struct Cli {
-    refname: String,
-    pre_rev: String,
-    new_rev: String,
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Run pipeline from file or directory on current HEAD
+    Run {
+        /// Path to pipeline file or directory
+        path: String,
+    },
 }
 
 fn main() -> anyhow::Result<()> {
+    if let Ok(_) = env::var("GIT_DIR") {
+        return run_hook();
+    }
+    let cli = Cli::parse();
+
+    Ok(())
+}
+
+fn run_hook() -> anyhow::Result<()> {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap_or_default();
     let args: Vec<String> = input.split_whitespace().map(String::from).collect();
